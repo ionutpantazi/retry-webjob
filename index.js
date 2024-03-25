d3afce02c2cd4c1bfd7d247dce0be8b513002137
ionut <ionut@vocala.co>
2024-03-25 17:30:04 +0000

    test change

---
+++ index.js
@@ -0,0 +1,7 @@
+const username = "\$ionutpantazi";
+const password = "nugCg1xFfDMauvlxYcBnhsLHezle4mggfMbPRoKnDjAdik4WHWcmR84uq86p";
+const log_url = 'https://ionutpantazi.scm.azurewebsites.net/api/triggeredwebjobs/err';
+const run_url = 'https://ionutpantazi.scm.azurewebsites.net/api/triggeredwebjobs/err/run';
+const log_path = 'C:\home\site\wwwroot\App_Data\jobs\triggered\debug\php.log';
+
+console.log(username);
