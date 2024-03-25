6751623d5ca98be9263cfd75a12c442d23e975f5
ionut <ionut@vocala.co>
2024-03-25 22:36:38 +0000

    server test

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const createHandler = require("azure-function-express").createHandler;
 const express = require("express");
 // Create express app as usual
 const app = express();
+const port = 3000;
 app.get("/api/hello-world", (req, res) => {
     res.json({
         status: "Welocme to Express App: Hello-world"
     });
+});
+// Start the Express server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 // Binds the express app to an Azure Function handler
 module.exports = createHandler(app);
